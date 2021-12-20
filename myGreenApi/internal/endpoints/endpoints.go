42d6c0ffa6d11de1fd7cdb8d543423d89cd7b396
Daniel Song <dsong.chan@gmail.com>
2021-12-19 21:29:31 -0800

    initial test commit for backend

---
+++ myGreenApi/internal/endpoints/endpoints.go
@@ -0,0 +1,14 @@
+package internal
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// fmt.Printf(w, "Hi there")
+}
+
+func test_import() {
+	fmt.Println("Test")
+}
---
+++ myGreenApi/internal/models/models.go
@@ -0,0 +1,6 @@
+package internal
+
+type greenDevice struct {
+	ID           string `json:"id"`
+	time_created string `json:"time_created"`
+}
